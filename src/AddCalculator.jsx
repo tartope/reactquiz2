e5ecc4579663a008314f65d27f1b9adcb607ed0b
Tunisia Artope <tartope@xpanxion.com>
2023-05-30 10:46:14 -0400

    Add AddCalculator componenet.

--- src/AddCalculator.jsx
+++ src/AddCalculator.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddCalculator = () => {
+    let [count, setCount] = useState(0);
+    let [total, setTotal] = useState(0);
+    console.log(count);
+
+    const handleAdd = (num)=>{
+        setCount(count + num);
+        setTotal(count);
+    }
 
     return (
         <>
-            <button>Add 1</button>
-            <button>Add 2</button>
-            <button>Add 3</button>
+            <button onClick={()=> handleAdd(1)}>Add 1</button>
+            <button onClick={()=> handleAdd(2)}>Add 2</button>
+            <button onClick={()=> handleAdd(3)}>Add 3</button>
             <br></br>
-            Total: 
+            Total: {count}
         </>
     )
 }
